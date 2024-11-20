93e3c0845be67540d3566ad8c589046c7b6062ba
Kumaran S G <85860051+KumaranSGK@users.noreply.github.com>
2024-11-20 16:58:09 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -3,19 +3,21 @@ package com.example.testapplication
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.testapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewModel: DataViewModel
+    private val viewModel: DataViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel.Amount(binding.amount.text.toString())
         viewModel.amount.observe(this) { amount ->
            if (amount){
              binding.numberLayout4.visibility= View.VISIBLE
